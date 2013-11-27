0fd66974943afcf2bf7563ea4d3c1f999d6d13e6
Whitney Levis <ms.whitney.levis@gmail.com>
2013-11-27 15:20:33 -0800

    Added yaml metadata date to posts and other things

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'yaml'
 
 class MyApp < Sinatra::Base
+
+  attr_accessor :posts
+
+  before do
+    @posts = Dir.glob("views/posts/*.erb").map do |post_name|
+      post_name.split("/").last.slice(0..-5)
+    end
+  end
 
   get "/" do
     erb :index
@@ -39,7 +48,24 @@ class MyApp < Sinatra::Base
   end
 
   get "/blog/:post_name" do
-    erb "/posts/#{params[:post_name]}".to_sym
+    page_content = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    content_no_meta = page_content.split("\n\n", 2).last
+    erb content_no_meta
   end
+
+  def meta_data
+    if @meta_data
+      @meta_data
+    else
+      @meta_data = {}
+      @posts.each do |post|
+        html = erb("/posts/#{post}".to_sym, layout: false)
+        meta = YAML.load(html.split("\n\n", 2).first)
+        @meta_data[post]=meta
+      end
+      @meta_data
+    end
+  end
+
 
 end
